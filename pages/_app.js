6ab911cb536693b954f22a34185d16cb0197202d
viniciusvolpe <viniciusvolpe44@gmail.com>
2021-01-25 19:42:03 +0000

    Class 1 - creating base components, adding styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,32 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import db from "../db.json";
 
 const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
-    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    font-family: 'Lato', sans-serif;
+    color: ${({ theme }) => theme.colors.contrastText};
   }
-`
+  html, body {
+    min-height: 100vh;
+  }
+  #__next {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+  }
+`;
 
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
+const theme = db.theme;
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
-  )
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Footer from "./Footer";
+import GitHubCorner from "./GitHubCorner";
+import QuizBackground from "./QuizBackground";
+import QuizLogo from "./QuizLogo";
+import Widget from "./Widget";
+
+export { Footer, GitHubCorner, QuizBackground, QuizLogo, Widget };
